36608704ea2b132e6f7662182402d237cda42343
Rashi Raffi <Rashi1281@gmail.com>
2020-12-23 13:39:15 +0530

    Dice Roller app updated

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,15 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener{
+        rollButton.setOnClickListener {
             rollDice()
         }
     }
@@ -25,8 +24,8 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-class Dice(val numSides: Int){
-    fun roll(): Int{
-        return(1..numSides).random()
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
